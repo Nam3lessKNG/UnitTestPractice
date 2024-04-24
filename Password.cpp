d94b64a257adf0fd41a311a38ce34f93bff004d5
Blake Hufford <huffordblake@gmail.com>
2024-04-24 11:05:47 -0700

    my work so far

--- Password.cpp
+++ Password.cpp
@@ -10,7 +10,7 @@ using std::string;
   'z' and any ASCII characters are allowed.
 */
 int Password::count_leading_characters(string phrase){
-  int repetition = 0;
+  int repetition = 1;
   int index = 0;
   while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
     repetition++;
@@ -19,4 +19,51 @@ int Password::count_leading_characters(string phrase){
   return repetition;
 }
 
+bool Password::has_mixed_case(string phrase){
+  int index = 0;
+  bool upper = false;
+  bool lower = false;
+  while( index < phrase.length()-1 ){
+    if(phrase[index] >= 'A' && phrase[index] <= 'Z'){
+      upper = true;
+    }
+    else if(phrase[index] >= 'a' && phrase[index] <= 'z'){
+      lower = true;
+    }
+  }
 
+  if(upper && lower){
+    return true;
+  }
+  return false;
+}
+
+Password::Password(){
+  pass_history.clear();
+  pass_history.push_back("ChicoCA-95929");
+}
+
+void Password::set(string phrase){
+  // DDigimon
+  //The password is at least 8 letters long, but no longer than 20
+  if(phrase.length() >= 8 && phrase.length() <= 20){
+    //It has no more than 3 of the same leading characters
+    if(count_leading_characters(phrase) <= 3){
+      //It has mixed case (at least one upper case and at least one lower case)
+      if(has_mixed_case(phrase)){
+        //It was not a previous password in the history
+        if(phrase != pass_history.at(0)){
+          pass_history.clear();
+          pass_history.push_back(phrase);
+        }
+      }
+    }
+  }
+}
+
+bool Password::authenticate(string phrase){
+  if(phrase == pass_history.at(0)){
+    return true;
+  }
+  return false;
+}
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -14,14 +14,14 @@ class PracticeTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PasswordTest, single_letter_password)
+TEST(PasswordTest, leading_single_letter)
 {
 	Password my_password;
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
 
-TEST(PasswordTest, double_letter_password)
+TEST(PasswordTest, leading_double_letter)
 {
 	Password my_password;
 	int actual = my_password.count_leading_characters("ZZ");
@@ -31,13 +31,57 @@ TEST(PasswordTest, double_letter_password)
 TEST(PasswordTest, alternating_letter_password)
 {
 	Password my_password;
-	int actual = my_password.count_leading_characters("ZzZ");
-	ASSERT_EQ(1, actual);
+	bool actual = my_password.has_mixed_case("ZzZ");
+	ASSERT_EQ(true, actual);
 }
 
-TEST(PasswordTest, empty_password)
+TEST(PasswordTest, middle_mix)
 {
 	Password my_password;
-	int actual = my_password.count_leading_characters("");
-	ASSERT_EQ(1, actual);
+	bool actual = my_password.has_mixed_case("ZZZaaZZZ");
+	ASSERT_EQ(true, actual);
+}
+
+TEST(PasswordTest, non_characters)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("$%!@");
+	ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, mixed_empty_password)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("");
+	ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, start_lower)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("aA");
+	ASSERT_EQ(true, actual);
+}
+
+TEST(PasswordTest, only_lower)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("a$");
+	ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, invalid_password)
+{
+	Password my_password;
+	my_password.set("a$");
+	bool actual = my_password.authenticate("a$");
+	ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, valid_password)
+{
+	Password my_password;
+	my_password.set("DDigimon");
+	bool actual = my_password.authenticate("DDigimon");
+	ASSERT_EQ(false, actual);
 }
